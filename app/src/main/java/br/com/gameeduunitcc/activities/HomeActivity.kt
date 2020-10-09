585ca21c70e80f32111943f4972022dae6ded9ac
Lucas Petruceli <lucas.mattos@redeconekta.com.br>
2020-10-09 19:23:56 -0300

    adicionando background HomeActivity + style fullScree + minApi 24

--- app/src/main/java/br/com/gameeduunitcc/activities/HomeActivity.kt
+++ app/src/main/java/br/com/gameeduunitcc/activities/HomeActivity.kt
@@ -1,16 +1,23 @@
 package br.com.gameeduunitcc.activities
 
+import android.content.Context
 import android.content.Intent
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import br.com.gameeduunitcc.R
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//        setBackground(applicationContext, rlHome, R.drawable.home_background)
         setupActivity()
     }
 
@@ -41,5 +48,17 @@ class HomeActivity : AppCompatActivity() {
     fun cadastrar() {
         val intent = Intent(this, CadastroActivity::class.java)
         startActivity(intent)
+    }
+
+    fun setBackground(context: Context, view: View, drawableId: Int) {
+//        TODO: performace sem a imagem é bem melhor (adicionar uma splash screen no caso de manter a imagem)
+//        TODO: Fazer teste de performarce setando a imagem direto no arquivo xml do layout
+
+        var bitmap = BitmapFactory.decodeResource(context.resources, drawableId)
+        val width: Int = Resources.getSystem().displayMetrics.widthPixels
+        val height: Int = Resources.getSystem().displayMetrics.heightPixels
+        bitmap = Bitmap.createScaledBitmap(bitmap!!, width, height, true)
+        val bitmapDrawable = BitmapDrawable(context.resources, bitmap)
+        view.background = bitmapDrawable
     }
 }
