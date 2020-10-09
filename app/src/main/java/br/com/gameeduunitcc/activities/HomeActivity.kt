cc79c6f51a45915d71566ac0ee6a73126bc028be
Lucas Petruceli <lucas.mattos@redeconekta.com.br>
2020-10-09 20:08:56 -0300

    novo background HomeActivity (mais leve)

--- app/src/main/java/br/com/gameeduunitcc/activities/HomeActivity.kt
+++ app/src/main/java/br/com/gameeduunitcc/activities/HomeActivity.kt
@@ -17,7 +17,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        setBackground(applicationContext, rlHome, R.drawable.home_background)
+        setBackground(applicationContext, rlHome, R.drawable.home_background_azul)
         setupActivity()
     }
 
